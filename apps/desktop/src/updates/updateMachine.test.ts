0bkevin/t3code

fix(desktop): show newest changes in nightly previews (#9138)

apps/desktop/src/updates/updateMachine.test.ts
@@ -62,7 +62,8 @@ describe("updateMachine", () => {
       status: "downloaded" as const,
       availableVersion: "1.1.0",
       downloadedVersion: "1.1.0",
-      releaseNotes: [{ version: "1.1.0", items: ["fix: queued update"] }],
+      releaseNotes: [{ version: "1.1.0", items: ["fix: queued update"], totalItems: 1 }],
+      omittedReleaseCount: 2,
       downloadPercent: 100,
     };
     const checking = reduceDesktopUpdateStateOnCheckStart(
@@ -78,14 +79,16 @@ describe("updateMachine", () => {
     expect(checking.status).toBe("checking");
     expect(checking.downloadedVersion).toBe("1.1.0");
     expect(checking.releaseNotes).toEqual(downloadedState.releaseNotes);
+    expect(checking.omittedReleaseCount).toBe(2);
     expect(failed.status).toBe("downloaded");
     expect(failed.downloadedVersion).toBe("1.1.0");
     expect(failed.releaseNotes).toEqual(downloadedState.releaseNotes);
+    expect(failed.omittedReleaseCount).toBe(2);
     expect(failed.message).toBeNull();
   });
 
   it("keeps the installer when the feed still offers its version", () => {
-    const releaseNotes = [{ version: "1.1.0", items: ["fix: queued update"] }];
+    const releaseNotes = [{ version: "1.1.0", items: ["fix: queued update"], totalItems: 1 }];
     const state = reduceDesktopUpdateStateOnUpdateAvailable(
       {
         ...createInitialDesktopUpdateState("1.0.0", runtimeInfo, "latest"),
@@ -94,6 +97,7 @@ describe("updateMachine", () => {
         availableVersion: "1.1.0",
         downloadedVersion: "1.1.0",
         releaseNotes,
+        omittedReleaseCount: 2,
         downloadPercent: 100,
       },
       "1.1.0",
@@ -103,6 +107,7 @@ describe("updateMachine", () => {
     expect(state.status).toBe("downloaded");
     expect(state.downloadedVersion).toBe("1.1.0");
     expect(state.releaseNotes).toEqual(releaseNotes);
+    expect(state.omittedReleaseCount).toBe(2);
     expect(state.downloadPercent).toBe(100);
   });
 
@@ -147,7 +152,7 @@ describe("updateMachine", () => {
   });
 
   it("preserves a downloaded update when no update is available", () => {
-    const releaseNotes = [{ version: "1.1.0", items: ["fix: queued update"] }];
+    const releaseNotes = [{ version: "1.1.0", items: ["fix: queued update"], totalItems: 1 }];
     const state = reduceDesktopUpdateStateOnNoUpdate(
       {
         ...createInitialDesktopUpdateState("1.0.0", runtimeInfo, "latest"),
@@ -156,6 +161,7 @@ describe("updateMachine", () => {
         availableVersion: "1.1.0",
         downloadedVersion: "1.1.0",
         releaseNotes,
+        omittedReleaseCount: 2,
         message: "old failure",
         errorContext: "download",
         canRetry: true,
@@ -167,6 +173,7 @@ describe("updateMachine", () => {
     expect(state.availableVersion).toBe("1.1.0");
     expect(state.downloadedVersion).toBe("1.1.0");
     expect(state.releaseNotes).toBe(releaseNotes);
+    expect(state.omittedReleaseCount).toBe(2);
     expect(state.downloadPercent).toBe(100);
     expect(state.message).toBeNull();
     expect(state.errorContext).toBeNull();
@@ -180,7 +187,8 @@ describe("updateMachine", () => {
         enabled: true,
         status: "error",
         availableVersion: "1.1.0",
-        releaseNotes: [{ version: "1.1.0", items: ["fix: stale update"] }],
+        releaseNotes: [{ version: "1.1.0", items: ["fix: stale update"], totalItems: 1 }],
+        omittedReleaseCount: 2,
         message: "old failure",
         errorContext: "download",
         canRetry: true,
@@ -192,6 +200,7 @@ describe("updateMachine", () => {
     expect(state.availableVersion).toBeNull();
     expect(state.downloadedVersion).toBeNull();
     expect(state.releaseNotes).toEqual([]);
+    expect(state.omittedReleaseCount).toBe(0);
     expect(state.message).toBeNull();
     expect(state.errorContext).toBeNull();
   });
@@ -201,6 +210,7 @@ describe("updateMachine", () => {
       {
         version: "1.1.0",
         items: ["feat: add update release notes"],
+        totalItems: 1,
       },
     ];
     const available = reduceDesktopUpdateStateOnUpdateAvailable(
@@ -212,6 +222,7 @@ describe("updateMachine", () => {
       "1.1.0",
       "2026-03-04T00:00:00.000Z",
       releaseNotes,
+      2,
     );
     const downloading = reduceDesktopUpdateStateOnDownloadStart(available);
     const progress = reduceDesktopUpdateStateOnDownloadProgress(downloading, 55.5);
@@ -219,6 +230,7 @@ describe("updateMachine", () => {
     expect(available.status).toBe("available");
     expect(available.channel).toBe("latest");
     expect(available.releaseNotes).toBe(releaseNotes);
+    expect(available.omittedReleaseCount).toBe(2);
     expect(downloading.releaseNotes).toBe(releaseNotes);
     expect(downloading.status).toBe("downloading");
     expect(downloading.downloadPercent).toBe(0);
@@ -233,11 +245,13 @@ describe("updateMachine", () => {
         enabled: true,
         status: "available",
         availableVersion: "1.1.0-nightly.1",
-        releaseNotes: [{ version: "1.1.0-nightly.1", items: ["feat: old note"] }],
+        releaseNotes: [{ version: "1.1.0-nightly.1", items: ["feat: old note"], totalItems: 1 }],
+        omittedReleaseCount: 2,
       },
       "2026-03-04T00:00:00.000Z",
     );
 
     expect(state.releaseNotes).toEqual([]);
+    expect(state.omittedReleaseCount).toBe(0);
   });
 });

apps/web/src/components/sidebar/SidebarUpdatePill.test.tsx
@@ -1,0 +1,86 @@
+import type { DesktopUpdateState } from "@t3tools/contracts";
+import { describe, expect, it, vi } from "vite-plus/test";
+
+import {
+  handleSidebarUpdateReleaseNotesPopoverOpenChange,
+  openSidebarUpdateReleaseNotesPopoverOnForwardTab,
+  shouldUseSidebarUpdateReleaseNotesPopover,
+} from "./SidebarUpdatePill";
+
+const nightlyState: DesktopUpdateState = {
+  enabled: true,
+  status: "available",
+  channel: "nightly",
+  currentVersion: "0.0.35",
+  hostArch: "arm64",
+  appArch: "arm64",
+  runningUnderArm64Translation: false,
+  availableVersion: "0.0.36-nightly.3",
+  downloadedVersion: null,
+  releaseNotes: [{ version: "0.0.36-nightly.3", items: ["Newest change"], totalItems: 1 }],
+  omittedReleaseCount: 0,
+  downloadPercent: null,
+  checkedAt: null,
+  message: null,
+  errorContext: null,
+  canRetry: false,
+};
+
+describe("sidebar update release notes popover", () => {
+  it("uses the popover only for visible nightly release notes", () => {
+    expect(shouldUseSidebarUpdateReleaseNotesPopover(true, nightlyState)).toBe(true);
+    expect(shouldUseSidebarUpdateReleaseNotesPopover(false, nightlyState)).toBe(false);
+    expect(
+      shouldUseSidebarUpdateReleaseNotesPopover(true, {
+        ...nightlyState,
+        channel: "latest",
+      }),
+    ).toBe(false);
+    expect(
+      shouldUseSidebarUpdateReleaseNotesPopover(true, {
+        ...nightlyState,
+        releaseNotes: [],
+      }),
+    ).toBe(false);
+  });
+
+  it("cancels trigger presses without canceling other open reasons", () => {
+    const cancelTriggerPress = vi.fn();
+    const cancelHover = vi.fn();
+
+    handleSidebarUpdateReleaseNotesPopoverOpenChange(true, {
+      reason: "trigger-press",
+      cancel: cancelTriggerPress,
+    });
+    handleSidebarUpdateReleaseNotesPopoverOpenChange(true, {
+      reason: "trigger-hover",
+      cancel: cancelHover,
+    });
+
+    expect(cancelTriggerPress).toHaveBeenCalledOnce();
+    expect(cancelHover).not.toHaveBeenCalled();
+  });
+
+  it("promotes forward Tab without preventing native navigation", () => {
+    const open = vi.fn();
+    const preventDefault = vi.fn();
+    const event = { key: "Tab", shiftKey: false, preventDefault };
+
+    openSidebarUpdateReleaseNotesPopoverOnForwardTab(event, { open }, "nightly-release-notes");
+
+    expect(open).toHaveBeenCalledWith("nightly-release-notes");
+    expect(preventDefault).not.toHaveBeenCalled();
+  });
+
+  it("does not promote backward Tab", () => {
+    const open = vi.fn();
+
+    openSidebarUpdateReleaseNotesPopoverOnForwardTab(
+      { key: "Tab", shiftKey: true },
+      { open },
+      "nightly-release-notes",
+    );
+
+    expect(open).not.toHaveBeenCalled();
+  });
+});

apps/web/src/components/sidebar/SidebarUpdateReleaseNotes.tsx
@@ -1,0 +1,120 @@
+import type { DesktopBridge, DesktopUpdateState } from "@t3tools/contracts";
+import { ExternalLinkIcon } from "lucide-react";
+
+import {
+  getDesktopUpdateReleaseHistoryUrl,
+  getDesktopUpdateReleaseUrl,
+} from "../desktopUpdate.logic";
+import { openDesktopUpdateReleaseNotes } from "../desktopUpdate.toast";
+import { Separator } from "../ui/separator";
+
+type DesktopUpdateShell = Pick<DesktopBridge, "openExternal">;
+
+function keyReleaseNoteItems(items: ReadonlyArray<string>) {
+  const occurrences = new Map<string, number>();
+  return items.map((item) => {
+    const occurrence = occurrences.get(item) ?? 0;
+    occurrences.set(item, occurrence + 1);
+    return { item, key: JSON.stringify([item, occurrence]) };
+  });
+}
+
+function ReleaseLink({
+  children,
+  releaseUrl,
+  shell,
+}: {
+  readonly children: string;
+  readonly releaseUrl: string;
+  readonly shell: DesktopUpdateShell | undefined;
+}) {
+  return (
+    <a
+      className="mt-2 inline-flex items-center gap-1 rounded-sm text-xs leading-5 text-muted-foreground underline decoration-dotted underline-offset-4 outline-none transition-colors hover:text-foreground focus-visible:text-foreground focus-visible:ring-2 focus-visible:ring-ring"
+      href={releaseUrl}
+      onClick={(event) => {
+        event.preventDefault();
+        void openDesktopUpdateReleaseNotes(shell, releaseUrl);
+      }}
+    >
+      {children}
+      <ExternalLinkIcon aria-hidden className="size-3 shrink-0" strokeWidth={2.25} />
+    </a>
+  );
+}
+
+export function SidebarUpdateReleaseNotes({
+  shell,
+  state,
+  tooltip,
+}: {
+  readonly shell: DesktopUpdateShell | undefined;
+  readonly state: DesktopUpdateState;
+  readonly tooltip: string;
+}) {
+  if (state.channel !== "nightly" || state.releaseNotes.length === 0) {
+    return <>{tooltip}</>;
+  }
+
+  return (
+    <div className="flex max-h-[calc(var(--available-height)-0.5rem)] min-h-0 w-fit max-w-[min(24rem,calc(100vw-2rem))] flex-col text-left">
+      <div className="shrink-0 px-1">
+        {state.status === "available" ? (
+          <div>
+            <div className="whitespace-nowrap text-sm leading-5 font-medium">
+              Update ready to download
+            </div>
+            {state.availableVersion ? (
+              <div className="mt-0.5 text-xs leading-4 text-muted-foreground">
+                {state.availableVersion}
+              </div>
+            ) : null}
+          </div>
+        ) : (
+          <div className="text-sm leading-5 font-medium">{tooltip}</div>
+        )}
+      </div>
+      <div className="min-h-0 max-h-[min(28rem,calc(100vh-6rem))] overflow-y-auto px-1 pt-4 pb-1">
+        {state.releaseNotes.map((releaseNote, index) => {
+          const releaseUrl = getDesktopUpdateReleaseUrl(releaseNote.version);
+          const omittedItemCount = Math.max(0, releaseNote.totalItems - releaseNote.items.length);
+          const linkLabel =
+            omittedItemCount === 0
+              ? "View release on GitHub"
+              : `${omittedItemCount} more ${omittedItemCount === 1 ? "change" : "changes"} on GitHub`;
+
+          return (
+            <div key={releaseNote.version}>
+              {index > 0 && <Separator className="my-3 bg-border/60" />}
+              <section>
+                <h3 className="text-foreground text-xs leading-4 font-semibold">
+                  {index === 0 ? "What's changed" : `Changes in ${releaseNote.version}`}
+                </h3>
+                <ul className="mt-2 space-y-1.5 pl-4 text-xs leading-5 text-popover-foreground/90">
+                  {keyReleaseNoteItems(releaseNote.items).map(({ item, key }) => (
+                    <li className="list-disc break-words" key={key}>
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+                {releaseUrl ? (
+                  <ReleaseLink releaseUrl={releaseUrl} shell={shell}>
+                    {linkLabel}
+                  </ReleaseLink>
+                ) : null}
+              </section>
+            </div>
+          );
+        })}
+        {state.omittedReleaseCount > 0 ? (
+          <div>
+            <Separator className="my-3 bg-border/60" />
+            <ReleaseLink releaseUrl={getDesktopUpdateReleaseHistoryUrl()} shell={shell}>
+              {`${state.omittedReleaseCount} older ${state.omittedReleaseCount === 1 ? "release" : "releases"} on GitHub`}
+            </ReleaseLink>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+}

richardsolomou/ras-code

(cherry picked from commit 5014e5fcdd6e5f4a53d0fea4cbecba733821a854)

apps/desktop/src/updates/updateMachine.test.ts
@@ -62,7 +62,8 @@ describe("updateMachine", () => {
       status: "downloaded" as const,
       availableVersion: "1.1.0",
       downloadedVersion: "1.1.0",
-      releaseNotes: [{ version: "1.1.0", items: ["fix: queued update"] }],
+      releaseNotes: [{ version: "1.1.0", items: ["fix: queued update"], totalItems: 1 }],
+      omittedReleaseCount: 2,
       downloadPercent: 100,
     };
     const checking = reduceDesktopUpdateStateOnCheckStart(
@@ -78,14 +79,16 @@ describe("updateMachine", () => {
     expect(checking.status).toBe("checking");
     expect(checking.downloadedVersion).toBe("1.1.0");
     expect(checking.releaseNotes).toEqual(downloadedState.releaseNotes);
+    expect(checking.omittedReleaseCount).toBe(2);
     expect(failed.status).toBe("downloaded");
     expect(failed.downloadedVersion).toBe("1.1.0");
     expect(failed.releaseNotes).toEqual(downloadedState.releaseNotes);
+    expect(failed.omittedReleaseCount).toBe(2);
     expect(failed.message).toBeNull();
   });
 
   it("keeps the installer when the feed still offers its version", () => {
-    const releaseNotes = [{ version: "1.1.0", items: ["fix: queued update"] }];
+    const releaseNotes = [{ version: "1.1.0", items: ["fix: queued update"], totalItems: 1 }];
     const state = reduceDesktopUpdateStateOnUpdateAvailable(
       {
         ...createInitialDesktopUpdateState("1.0.0", runtimeInfo, "latest"),
@@ -94,6 +97,7 @@ describe("updateMachine", () => {
         availableVersion: "1.1.0",
         downloadedVersion: "1.1.0",
         releaseNotes,
+        omittedReleaseCount: 2,
         downloadPercent: 100,
       },
       "1.1.0",
@@ -103,6 +107,7 @@ describe("updateMachine", () => {
     expect(state.status).toBe("downloaded");
     expect(state.downloadedVersion).toBe("1.1.0");
     expect(state.releaseNotes).toEqual(releaseNotes);
+    expect(state.omittedReleaseCount).toBe(2);
     expect(state.downloadPercent).toBe(100);
   });
 
@@ -147,7 +152,7 @@ describe("updateMachine", () => {
   });
 
   it("preserves a downloaded update when no update is available", () => {
-    const releaseNotes = [{ version: "1.1.0", items: ["fix: queued update"] }];
+    const releaseNotes = [{ version: "1.1.0", items: ["fix: queued update"], totalItems: 1 }];
     const state = reduceDesktopUpdateStateOnNoUpdate(
       {
         ...createInitialDesktopUpdateState("1.0.0", runtimeInfo, "latest"),
@@ -156,6 +161,7 @@ describe("updateMachine", () => {
         availableVersion: "1.1.0",
         downloadedVersion: "1.1.0",
         releaseNotes,
+        omittedReleaseCount: 2,
         message: "old failure",
         errorContext: "download",
         canRetry: true,
@@ -167,6 +173,7 @@ describe("updateMachine", () => {
     expect(state.availableVersion).toBe("1.1.0");
     expect(state.downloadedVersion).toBe("1.1.0");
     expect(state.releaseNotes).toBe(releaseNotes);
+    expect(state.omittedReleaseCount).toBe(2);
     expect(state.downloadPercent).toBe(100);
     expect(state.message).toBeNull();
     expect(state.errorContext).toBeNull();
@@ -180,7 +187,8 @@ describe("updateMachine", () => {
         enabled: true,
         status: "error",
         availableVersion: "1.1.0",
-        releaseNotes: [{ version: "1.1.0", items: ["fix: stale update"] }],
+        releaseNotes: [{ version: "1.1.0", items: ["fix: stale update"], totalItems: 1 }],
+        omittedReleaseCount: 2,
         message: "old failure",
         errorContext: "download",
         canRetry: true,
@@ -192,6 +200,7 @@ describe("updateMachine", () => {
     expect(state.availableVersion).toBeNull();
     expect(state.downloadedVersion).toBeNull();
     expect(state.releaseNotes).toEqual([]);
+    expect(state.omittedReleaseCount).toBe(0);
     expect(state.message).toBeNull();
     expect(state.errorContext).toBeNull();
   });
@@ -201,6 +210,7 @@ describe("updateMachine", () => {
       {
         version: "1.1.0",
         items: ["feat: add update release notes"],
+        totalItems: 1,
       },
     ];
     const available = reduceDesktopUpdateStateOnUpdateAvailable(
@@ -212,6 +222,7 @@ describe("updateMachine", () => {
       "1.1.0",
       "2026-03-04T00:00:00.000Z",
       releaseNotes,
+      2,
     );
     const downloading = reduceDesktopUpdateStateOnDownloadStart(available);
     const progress = reduceDesktopUpdateStateOnDownloadProgress(downloading, 55.5);
@@ -219,6 +230,7 @@ describe("updateMachine", () => {
     expect(available.status).toBe("available");
     expect(available.channel).toBe("latest");
     expect(available.releaseNotes).toBe(releaseNotes);
+    expect(available.omittedReleaseCount).toBe(2);
     expect(downloading.releaseNotes).toBe(releaseNotes);
     expect(downloading.status).toBe("downloading");
     expect(downloading.downloadPercent).toBe(0);
@@ -233,11 +245,13 @@ describe("updateMachine", () => {
         enabled: true,
         status: "available",
         availableVersion: "1.1.0-canary.1",
-        releaseNotes: [{ version: "1.1.0-canary.1", items: ["feat: old note"] }],
+        releaseNotes: [{ version: "1.1.0-canary.1", items: ["feat: old note"], totalItems: 1 }],
+        omittedReleaseCount: 2,
       },
       "2026-03-04T00:00:00.000Z",
     );
 
     expect(state.releaseNotes).toEqual([]);
+    expect(state.omittedReleaseCount).toBe(0);
   });
 });

apps/web/src/components/sidebar/SidebarUpdatePill.test.tsx
@@ -1,0 +1,86 @@
+import type { DesktopUpdateState } from "@ras-code/contracts";
+import { describe, expect, it, vi } from "vite-plus/test";
+
+import {
+  handleSidebarUpdateReleaseNotesPopoverOpenChange,
+  openSidebarUpdateReleaseNotesPopoverOnForwardTab,
+  shouldUseSidebarUpdateReleaseNotesPopover,
+} from "./SidebarUpdatePill";
+
+const nightlyState: DesktopUpdateState = {
+  enabled: true,
+  status: "available",
+  channel: "canary",
+  currentVersion: "0.0.35",
+  hostArch: "arm64",
+  appArch: "arm64",
+  runningUnderArm64Translation: false,
+  availableVersion: "0.0.36-nightly.3",
+  downloadedVersion: null,
+  releaseNotes: [{ version: "0.0.36-nightly.3", items: ["Newest change"], totalItems: 1 }],
+  omittedReleaseCount: 0,
+  downloadPercent: null,
+  checkedAt: null,
+  message: null,
+  errorContext: null,
+  canRetry: false,
+};
+
+describe("sidebar update release notes popover", () => {
+  it("uses the popover only for visible canary release notes", () => {
+    expect(shouldUseSidebarUpdateReleaseNotesPopover(true, nightlyState)).toBe(true);
+    expect(shouldUseSidebarUpdateReleaseNotesPopover(false, nightlyState)).toBe(false);
+    expect(
+      shouldUseSidebarUpdateReleaseNotesPopover(true, {
+        ...nightlyState,
+        channel: "latest",
+      }),
+    ).toBe(false);
+    expect(
+      shouldUseSidebarUpdateReleaseNotesPopover(true, {
+        ...nightlyState,
+        releaseNotes: [],
+      }),
+    ).toBe(false);
+  });
+
+  it("cancels trigger presses without canceling other open reasons", () => {
+    const cancelTriggerPress = vi.fn();
+    const cancelHover = vi.fn();
+
+    handleSidebarUpdateReleaseNotesPopoverOpenChange(true, {
+      reason: "trigger-press",
+      cancel: cancelTriggerPress,
+    });
+    handleSidebarUpdateReleaseNotesPopoverOpenChange(true, {
+      reason: "trigger-hover",
+      cancel: cancelHover,
+    });
+
+    expect(cancelTriggerPress).toHaveBeenCalledOnce();
+    expect(cancelHover).not.toHaveBeenCalled();
+  });
+
+  it("promotes forward Tab without preventing native navigation", () => {
+    const open = vi.fn();
+    const preventDefault = vi.fn();
+    const event = { key: "Tab", shiftKey: false, preventDefault };
+
+    openSidebarUpdateReleaseNotesPopoverOnForwardTab(event, { open }, "nightly-release-notes");
+
+    expect(open).toHaveBeenCalledWith("nightly-release-notes");
+    expect(preventDefault).not.toHaveBeenCalled();
+  });
+
+  it("does not promote backward Tab", () => {
+    const open = vi.fn();
+
+    openSidebarUpdateReleaseNotesPopoverOnForwardTab(
+      { key: "Tab", shiftKey: true },
+      { open },
+      "nightly-release-notes",
+    );
+
+    expect(open).not.toHaveBeenCalled();
+  });
+});

apps/web/src/components/sidebar/SidebarUpdateReleaseNotes.tsx
@@ -1,0 +1,120 @@
+import type { DesktopBridge, DesktopUpdateState } from "@ras-code/contracts";
+import { ExternalLinkIcon } from "lucide-react";
+
+import {
+  getDesktopUpdateReleaseHistoryUrl,
+  getDesktopUpdateReleaseUrl,
+} from "../desktopUpdate.logic";
+import { openDesktopUpdateReleaseNotes } from "../desktopUpdate.toast";
+import { Separator } from "../ui/separator";
+
+type DesktopUpdateShell = Pick<DesktopBridge, "openExternal">;
+
+function keyReleaseNoteItems(items: ReadonlyArray<string>) {
+  const occurrences = new Map<string, number>();
+  return items.map((item) => {
+    const occurrence = occurrences.get(item) ?? 0;
+    occurrences.set(item, occurrence + 1);
+    return { item, key: JSON.stringify([item, occurrence]) };
+  });
+}
+
+function ReleaseLink({
+  children,
+  releaseUrl,
+  shell,
+}: {
+  readonly children: string;
+  readonly releaseUrl: string;
+  readonly shell: DesktopUpdateShell | undefined;
+}) {
+  return (
+    <a
+      className="mt-2 inline-flex items-center gap-1 rounded-sm text-xs leading-5 text-muted-foreground underline decoration-dotted underline-offset-4 outline-none transition-colors hover:text-foreground focus-visible:text-foreground focus-visible:ring-2 focus-visible:ring-ring"
+      href={releaseUrl}
+      onClick={(event) => {
+        event.preventDefault();
+        void openDesktopUpdateReleaseNotes(shell, releaseUrl);
+      }}
+    >
+      {children}
+      <ExternalLinkIcon aria-hidden className="size-3 shrink-0" strokeWidth={2.25} />
+    </a>
+  );
+}
+
+export function SidebarUpdateReleaseNotes({
+  shell,
+  state,
+  tooltip,
+}: {
+  readonly shell: DesktopUpdateShell | undefined;
+  readonly state: DesktopUpdateState;
+  readonly tooltip: string;
+}) {
+  if (state.channel !== "canary" || state.releaseNotes.length === 0) {
+    return <>{tooltip}</>;
+  }
+
+  return (
+    <div className="flex max-h-[calc(var(--available-height)-0.5rem)] min-h-0 w-fit max-w-[min(24rem,calc(100vw-2rem))] flex-col text-left">
+      <div className="shrink-0 px-1">
+        {state.status === "available" ? (
+          <div>
+            <div className="whitespace-nowrap text-sm leading-5 font-medium">
+              Update ready to download
+            </div>
+            {state.availableVersion ? (
+              <div className="mt-0.5 text-xs leading-4 text-muted-foreground">
+                {state.availableVersion}
+              </div>
+            ) : null}
+          </div>
+        ) : (
+          <div className="text-sm leading-5 font-medium">{tooltip}</div>
+        )}
+      </div>
+      <div className="min-h-0 max-h-[min(28rem,calc(100vh-6rem))] overflow-y-auto px-1 pt-4 pb-1">
+        {state.releaseNotes.map((releaseNote, index) => {
+          const releaseUrl = getDesktopUpdateReleaseUrl(releaseNote.version);
+          const omittedItemCount = Math.max(0, releaseNote.totalItems - releaseNote.items.length);
+          const linkLabel =
+            omittedItemCount === 0
+              ? "View release on GitHub"
+              : `${omittedItemCount} more ${omittedItemCount === 1 ? "change" : "changes"} on GitHub`;
+
+          return (
+            <div key={releaseNote.version}>
+              {index > 0 && <Separator className="my-3 bg-border/60" />}
+              <section>
+                <h3 className="text-foreground text-xs leading-4 font-semibold">
+                  {index === 0 ? "What's changed" : `Changes in ${releaseNote.version}`}
+                </h3>
+                <ul className="mt-2 space-y-1.5 pl-4 text-xs leading-5 text-popover-foreground/90">
+                  {keyReleaseNoteItems(releaseNote.items).map(({ item, key }) => (
+                    <li className="list-disc break-words" key={key}>
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+                {releaseUrl ? (
+                  <ReleaseLink releaseUrl={releaseUrl} shell={shell}>
+                    {linkLabel}
+                  </ReleaseLink>
+                ) : null}
+              </section>
+            </div>
+          );
+        })}
+        {state.omittedReleaseCount > 0 ? (
+          <div>
+            <Separator className="my-3 bg-border/60" />
+            <ReleaseLink releaseUrl={getDesktopUpdateReleaseHistoryUrl()} shell={shell}>
+              {`${state.omittedReleaseCount} older ${state.omittedReleaseCount === 1 ? "release" : "releases"} on GitHub`}
+            </ReleaseLink>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+}