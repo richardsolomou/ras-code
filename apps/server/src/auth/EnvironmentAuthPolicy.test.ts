nico2525nn/t3code

fix(server): isolate remote web session cookies (#8085)
Co-authored-by: Julius Marminge <julius0216@outlook.com>

apps/server/src/auth/EnvironmentAuthPolicy.test.ts
@@ -4,12 +4,14 @@ import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
 
 import * as ServerConfig from "../config.ts";
+import * as ServerEnvironment from "../environment/ServerEnvironment.ts";
 import * as EnvironmentAuthPolicy from "./EnvironmentAuthPolicy.ts";
 
 const makeEnvironmentAuthPolicyLayer = (
   overrides?: Partial<ServerConfig.ServerConfig["Service"]>,
 ) =>
   EnvironmentAuthPolicy.layer.pipe(
+    Layer.provide(ServerEnvironment.identityLayer),
     Layer.provide(
       Layer.effect(
         ServerConfig.ServerConfig,
@@ -107,7 +109,7 @@ it.layer(NodeServices.layer)("EnvironmentAuthPolicy.layer", (it) => {
 
       expect(descriptor.policy).toBe("remote-reachable");
       expect(descriptor.bootstrapMethods).toEqual(["one-time-token"]);
-      expect(descriptor.sessionCookieName).toBe("t3_session");
+      expect(descriptor.sessionCookieName).toMatch(/^t3_session_[a-f0-9]{12}$/);
     }).pipe(
       Effect.provide(
         makeEnvironmentAuthPolicyLayer({
@@ -143,7 +145,7 @@ it.layer(NodeServices.layer)("EnvironmentAuthPolicy.layer", (it) => {
       const descriptor = yield* policy.getDescriptor();
 
       expect(descriptor.policy).toBe("remote-reachable");
-      expect(descriptor.sessionCookieName).toBe("t3_session");
+      expect(descriptor.sessionCookieName).toMatch(/^t3_session_[a-f0-9]{12}$/);
     }).pipe(
       Effect.provide(
         makeEnvironmentAuthPolicyLayer({

apps/server/src/auth/SessionStore.test.ts
@@ -1,4 +1,5 @@
 import * as NodeServices from "@effect/platform-node/NodeServices";
+import { EnvironmentId } from "@t3tools/contracts";
 import { expect, it } from "@effect/vitest";
 import * as Duration from "effect/Duration";
 import * as Effect from "effect/Effect";
@@ -7,15 +8,14 @@ import * as TestClock from "effect/testing/TestClock";
 import * as SqlClient from "effect/unstable/sql/SqlClient";
 
 import * as ServerConfig from "../config.ts";
+import * as ServerEnvironment from "../environment/ServerEnvironment.ts";
 import { PersistenceSqlError } from "../persistence/Errors.ts";
 import { SqlitePersistenceMemory } from "../persistence/Layers/Sqlite.ts";
 import * as AuthSessions from "../persistence/AuthSessions.ts";
 import * as SessionStore from "./SessionStore.ts";
 import * as ServerSecretStore from "./ServerSecretStore.ts";
 
-const makeServerConfigLayer = (
-  overrides?: Partial<Pick<ServerConfig.ServerConfig["Service"], "desktopBootstrapToken">>,
-) =>
+const makeServerConfigLayer = (overrides?: Partial<ServerConfig.ServerConfig["Service"]>) =>
   Layer.effect(
     ServerConfig.ServerConfig,
     Effect.gen(function* () {
@@ -27,12 +27,19 @@ const makeServerConfigLayer = (
     }),
   ).pipe(Layer.provide(ServerConfig.layerTest(process.cwd(), { prefix: "t3-auth-session-test-" })));
 
+const makeServerEnvironmentLayer = (environmentId: EnvironmentId) =>
+  Layer.succeed(ServerEnvironment.ServerEnvironmentIdentity, {
+    getEnvironmentId: Effect.succeed(environmentId),
+  });
+
 const makeSessionStoreLayer = (
-  overrides?: Partial<Pick<ServerConfig.ServerConfig["Service"], "desktopBootstrapToken">>,
+  overrides?: Partial<ServerConfig.ServerConfig["Service"]>,
+  environmentId = EnvironmentId.make("test-environment"),
 ) =>
   SessionStore.layer.pipe(
     Layer.provide(SqlitePersistenceMemory),
     Layer.provide(ServerSecretStore.layer),
+    Layer.provide(makeServerEnvironmentLayer(environmentId)),
     Layer.provide(makeServerConfigLayer(overrides)),
   );
 
@@ -58,10 +65,32 @@ const failingSessionLookupCredentialLayer = Layer.effect(
   Layer.provide(failingSessionLookupRepositoryLayer),
   Layer.provide(ServerSecretStore.layer),
   Layer.provide(SqlitePersistenceMemory),
+  Layer.provide(makeServerEnvironmentLayer(EnvironmentId.make("test-environment"))),
   Layer.provide(makeServerConfigLayer()),
 );
 
 it.layer(NodeServices.layer)("SessionStore.layer", (it) => {
+  it.effect("keys remote cookies by environment identity instead of state directory", () =>
+    Effect.gen(function* () {
+      const cookieName = (stateDir: string, environmentId: EnvironmentId) =>
+        Effect.gen(function* () {
+          const sessions = yield* SessionStore.SessionStore;
+          return sessions.cookieName;
+        }).pipe(
+          Effect.provide(
+            makeSessionStoreLayer({ mode: "web", host: "192.168.1.50", stateDir }, environmentId),
+          ),
+        );
+
+      const original = yield* cookieName("/srv/t3-one", EnvironmentId.make("environment-one"));
+      const moved = yield* cookieName("/srv/t3-moved", EnvironmentId.make("environment-one"));
+      const other = yield* cookieName("/srv/t3-one", EnvironmentId.make("environment-two"));
+
+      expect(moved).toBe(original);
+      expect(other).not.toBe(original);
+    }),
+  );
+
   it.effect("issues and verifies signed browser session tokens", () =>
     Effect.gen(function* () {
       const sessions = yield* SessionStore.SessionStore;

apps/server/src/auth/utils.test.ts
@@ -64,6 +64,7 @@ describe("session cookie isolation", () => {
       port: 5775,
       host: "127.0.0.1",
       instanceKey: "/tmp/t3-agent-one",
+      environmentId: "environment-one",
       development: true,
     });
     const second = resolveSessionCookieName({
@@ -71,6 +72,7 @@ describe("session cookie isolation", () => {
       port: 5775,
       host: "127.0.0.1",
       instanceKey: "/tmp/t3-agent-two",
+      environmentId: "environment-two",
       development: true,
     });
 
@@ -79,25 +81,48 @@ describe("session cookie isolation", () => {
     expect(first).not.toBe(second);
   });
 
-  it("keeps the hosted web cookie stable across server instances", () => {
-    expect(
-      resolveSessionCookieName({
-        mode: "web",
-        port: 8080,
-        host: "0.0.0.0",
-        instanceKey: "/srv/release-a",
-        development: false,
-      }),
-    ).toBe("t3_session");
-    expect(
-      resolveSessionCookieName({
-        mode: "web",
-        port: 9090,
-        host: "app.example.com",
-        instanceKey: "/srv/release-b",
-        development: false,
-      }),
-    ).toBe("t3_session");
+  it("isolates remote web servers by server state", () => {
+    const first = resolveSessionCookieName({
+      mode: "web",
+      port: 3773,
+      host: "192.168.1.50",
+      instanceKey: "/srv/t3-one",
+      environmentId: "environment-one",
+      development: false,
+    });
+    const second = resolveSessionCookieName({
+      mode: "web",
+      port: 5775,
+      host: "192.168.1.50",
+      instanceKey: "/srv/t3-two",
+      environmentId: "environment-two",
+      development: false,
+    });
+
+    expect(first).toMatch(/^t3_session_[a-f0-9]{12}$/);
+    expect(second).toMatch(/^t3_session_[a-f0-9]{12}$/);
+    expect(first).not.toBe(second);
+  });
+
+  it("keeps a remote web server cookie stable across port changes", () => {
+    const first = resolveSessionCookieName({
+      mode: "web",
+      port: 8080,
+      host: "0.0.0.0",
+      instanceKey: "/srv/t3",
+      environmentId: "environment-one",
+      development: false,
+    });
+    const second = resolveSessionCookieName({
+      mode: "web",
+      port: 9090,
+      host: "app.example.com",
+      instanceKey: "/srv/t3",
+      environmentId: "environment-one",
+      development: false,
+    });
+
+    expect(first).toBe(second);
   });
 
   it("retains desktop port scoping", () => {
@@ -107,6 +132,7 @@ describe("session cookie isolation", () => {
         port: 3773,
         host: "127.0.0.1",
         instanceKey: "/tmp/desktop",
+        environmentId: "environment-one",
         development: true,
       }),
     ).toBe("t3_session_3773");
@@ -119,6 +145,7 @@ describe("session cookie isolation", () => {
         port: 5775,
         host: "0.0.0.0",
         instanceKey: "/tmp/t3-wildcard-dev",
+        environmentId: "environment-one",
         development: true,
       }),
     ).toMatch(/^t3_session_5775_[a-f0-9]{12}$/);

richardsolomou/ras-code

(cherry picked from commit c78ae50a5a5fdf8f42d0aaa0103b26ee836f0cfc)

apps/server/src/auth/EnvironmentAuthPolicy.test.ts
@@ -4,12 +4,14 @@ import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
 
 import * as ServerConfig from "../config.ts";
+import * as ServerEnvironment from "../environment/ServerEnvironment.ts";
 import * as EnvironmentAuthPolicy from "./EnvironmentAuthPolicy.ts";
 
 const makeEnvironmentAuthPolicyLayer = (
   overrides?: Partial<ServerConfig.ServerConfig["Service"]>,
 ) =>
   EnvironmentAuthPolicy.layer.pipe(
+    Layer.provide(ServerEnvironment.identityLayer),
     Layer.provide(
       Layer.effect(
         ServerConfig.ServerConfig,
@@ -109,7 +111,7 @@ it.layer(NodeServices.layer)("EnvironmentAuthPolicy.layer", (it) => {
 
       expect(descriptor.policy).toBe("remote-reachable");
       expect(descriptor.bootstrapMethods).toEqual(["one-time-token"]);
-      expect(descriptor.sessionCookieName).toBe("ras_session");
+      expect(descriptor.sessionCookieName).toMatch(/^ras_session_[a-f0-9]{12}$/);
     }).pipe(
       Effect.provide(
         makeEnvironmentAuthPolicyLayer({
@@ -145,7 +147,7 @@ it.layer(NodeServices.layer)("EnvironmentAuthPolicy.layer", (it) => {
       const descriptor = yield* policy.getDescriptor();
 
       expect(descriptor.policy).toBe("remote-reachable");
-      expect(descriptor.sessionCookieName).toBe("ras_session");
+      expect(descriptor.sessionCookieName).toMatch(/^ras_session_[a-f0-9]{12}$/);
     }).pipe(
       Effect.provide(
         makeEnvironmentAuthPolicyLayer({

apps/server/src/auth/SessionStore.test.ts
@@ -1,4 +1,5 @@
 import * as NodeServices from "@effect/platform-node/NodeServices";
+import { EnvironmentId } from "@ras-code/contracts";
 import { expect, it } from "@effect/vitest";
 import * as Duration from "effect/Duration";
 import * as Effect from "effect/Effect";
@@ -7,15 +8,14 @@ import * as TestClock from "effect/testing/TestClock";
 import * as SqlClient from "effect/unstable/sql/SqlClient";
 
 import * as ServerConfig from "../config.ts";
+import * as ServerEnvironment from "../environment/ServerEnvironment.ts";
 import { PersistenceSqlError } from "../persistence/Errors.ts";
 import { SqlitePersistenceMemory } from "../persistence/Layers/Sqlite.ts";
 import * as AuthSessions from "../persistence/AuthSessions.ts";
 import * as SessionStore from "./SessionStore.ts";
 import * as ServerSecretStore from "./ServerSecretStore.ts";
 
-const makeServerConfigLayer = (
-  overrides?: Partial<Pick<ServerConfig.ServerConfig["Service"], "desktopBootstrapToken">>,
-) =>
+const makeServerConfigLayer = (overrides?: Partial<ServerConfig.ServerConfig["Service"]>) =>
   Layer.effect(
     ServerConfig.ServerConfig,
     Effect.gen(function* () {
@@ -29,12 +29,19 @@ const makeServerConfigLayer = (
     Layer.provide(ServerConfig.layerTest(process.cwd(), { prefix: "ras-code-auth-session-test-" })),
   );
 
+const makeServerEnvironmentLayer = (environmentId: EnvironmentId) =>
+  Layer.succeed(ServerEnvironment.ServerEnvironmentIdentity, {
+    getEnvironmentId: Effect.succeed(environmentId),
+  });
+
 const makeSessionStoreLayer = (
-  overrides?: Partial<Pick<ServerConfig.ServerConfig["Service"], "desktopBootstrapToken">>,
+  overrides?: Partial<ServerConfig.ServerConfig["Service"]>,
+  environmentId = EnvironmentId.make("test-environment"),
 ) =>
   SessionStore.layer.pipe(
     Layer.provide(SqlitePersistenceMemory),
     Layer.provide(ServerSecretStore.layer),
+    Layer.provide(makeServerEnvironmentLayer(environmentId)),
     Layer.provide(makeServerConfigLayer(overrides)),
   );
 
@@ -60,10 +67,32 @@ const failingSessionLookupCredentialLayer = Layer.effect(
   Layer.provide(failingSessionLookupRepositoryLayer),
   Layer.provide(ServerSecretStore.layer),
   Layer.provide(SqlitePersistenceMemory),
+  Layer.provide(makeServerEnvironmentLayer(EnvironmentId.make("test-environment"))),
   Layer.provide(makeServerConfigLayer()),
 );
 
 it.layer(NodeServices.layer)("SessionStore.layer", (it) => {
+  it.effect("keys remote cookies by environment identity instead of state directory", () =>
+    Effect.gen(function* () {
+      const cookieName = (stateDir: string, environmentId: EnvironmentId) =>
+        Effect.gen(function* () {
+          const sessions = yield* SessionStore.SessionStore;
+          return sessions.cookieName;
+        }).pipe(
+          Effect.provide(
+            makeSessionStoreLayer({ mode: "web", host: "192.168.1.50", stateDir }, environmentId),
+          ),
+        );
+
+      const original = yield* cookieName("/srv/release-a", EnvironmentId.make("environment-one"));
+      const moved = yield* cookieName("/srv/release-moved", EnvironmentId.make("environment-one"));
+      const other = yield* cookieName("/srv/release-a", EnvironmentId.make("environment-two"));
+
+      expect(moved).toBe(original);
+      expect(other).not.toBe(original);
+    }),
+  );
+
   it.effect("issues and verifies signed browser session tokens", () =>
     Effect.gen(function* () {
       const sessions = yield* SessionStore.SessionStore;

apps/server/src/auth/utils.test.ts
@@ -64,6 +64,7 @@ describe("session cookie isolation", () => {
       port: 5775,
       host: "127.0.0.1",
       instanceKey: "/tmp/ras-code-agent-one",
+      environmentId: "environment-one",
       development: true,
     });
     const second = resolveSessionCookieName({
@@ -71,6 +72,7 @@ describe("session cookie isolation", () => {
       port: 5775,
       host: "127.0.0.1",
       instanceKey: "/tmp/ras-code-agent-two",
+      environmentId: "environment-two",
       development: true,
     });
 
@@ -79,25 +81,48 @@ describe("session cookie isolation", () => {
     expect(first).not.toBe(second);
   });
 
-  it("keeps the hosted web cookie stable across server instances", () => {
-    expect(
-      resolveSessionCookieName({
-        mode: "web",
-        port: 8080,
-        host: "0.0.0.0",
-        instanceKey: "/srv/release-a",
-        development: false,
-      }),
-    ).toBe("ras_session");
-    expect(
-      resolveSessionCookieName({
-        mode: "web",
-        port: 9090,
-        host: "app.example.com",
-        instanceKey: "/srv/release-b",
-        development: false,
-      }),
-    ).toBe("ras_session");
+  it("isolates remote web servers by server state", () => {
+    const first = resolveSessionCookieName({
+      mode: "web",
+      port: 3773,
+      host: "192.168.1.50",
+      instanceKey: "/srv/release-a",
+      environmentId: "environment-one",
+      development: false,
+    });
+    const second = resolveSessionCookieName({
+      mode: "web",
+      port: 5775,
+      host: "192.168.1.50",
+      instanceKey: "/srv/release-b",
+      environmentId: "environment-two",
+      development: false,
+    });
+
+    expect(first).toMatch(/^ras_session_[a-f0-9]{12}$/);
+    expect(second).toMatch(/^ras_session_[a-f0-9]{12}$/);
+    expect(first).not.toBe(second);
+  });
+
+  it("keeps a remote web server cookie stable across port changes", () => {
+    const first = resolveSessionCookieName({
+      mode: "web",
+      port: 8080,
+      host: "0.0.0.0",
+      instanceKey: "/srv/release-a",
+      environmentId: "environment-one",
+      development: false,
+    });
+    const second = resolveSessionCookieName({
+      mode: "web",
+      port: 9090,
+      host: "app.example.com",
+      instanceKey: "/srv/release-a",
+      environmentId: "environment-one",
+      development: false,
+    });
+
+    expect(first).toBe(second);
   });
 
   it("retains desktop port scoping", () => {
@@ -107,6 +132,7 @@ describe("session cookie isolation", () => {
         port: 3773,
         host: "127.0.0.1",
         instanceKey: "/tmp/desktop",
+        environmentId: "environment-one",
         development: true,
       }),
     ).toBe("ras_session_3773");
@@ -119,6 +145,7 @@ describe("session cookie isolation", () => {
         port: 5775,
         host: "0.0.0.0",
         instanceKey: "/tmp/ras-code-wildcard-dev",
+        environmentId: "environment-one",
         development: true,
       }),
     ).toMatch(/^ras_session_5775_[a-f0-9]{12}$/);