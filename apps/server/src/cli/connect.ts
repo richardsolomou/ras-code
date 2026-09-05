0bkevin/t3code

fix(connect): diagnose incomplete headless server setup (#9602)

apps/server/src/cli/connect.ts
@@ -35,6 +35,7 @@ import * as ServerSecretStore from "../auth/ServerSecretStore.ts";
 import * as BootService from "../cloud/bootService.ts";
 import * as CliState from "../cloud/CliState.ts";
 import * as CliTokenManager from "../cloud/CliTokenManager.ts";
+import { filterRelayResponse } from "../cloud/relayResponse.ts";
 import {
   CLOUD_LINKED_USER_ID,
   isAgentActivityPublishingEnabledValue,
@@ -208,6 +209,8 @@ function formatCloudStatus(status: CloudCliStatus, options?: { readonly json?: b
     `  Relay: ${status.relayUrl ?? "not provisioned"}`,
     `  Publish agent activity: ${status.publishAgentActivity ? "enabled" : "disabled"}`,
     ...formatRelayClientStatus(status.relayClient),
+    "",
+    "This is saved setup, not a live connection check. Check the background service with `t3 service status`.",
     ...(nextStep ? ["", `Next: ${nextStep}`] : []),
   ].join("\n");
 }
@@ -346,7 +349,7 @@ const unlinkRelayEnvironment = Effect.fn("cloud.cli.unlink_relay_environment")(f
   ).pipe(
     HttpClientRequest.bearerToken(token.value.accessToken),
     httpClient.execute,
-    Effect.flatMap(HttpClientResponse.filterStatusOk),
+    Effect.flatMap(filterRelayResponse),
     Effect.flatMap(HttpClientResponse.schemaBodyJson(RelayOkResponse)),
     withRelayClientTracing,
   );
@@ -689,17 +692,17 @@ export const connectCommand = Command.make("connect", {
         // Show which account was linked so an unexpected identity (an
         // authorization code for a different account) is visible before the
         // machine is brought online.
-        yield* Console.log(`✓ Connected${connectedAs(linked.identity)}`);
+        yield* Console.log(`✓ Authorized${connectedAs(linked.identity)}`);
 
-        // Connect itself already succeeded; a boot-service failure must not
-        // fail the command, just tell the user what happened and move on.
+        // Authorization is stored. If service setup fails, preserve it and
+        // show how to run the server manually.
         const background = yield* recoverServiceOnboardingOffer(offerServiceDuringOnboarding);
         if (background) {
           const platform = yield* HostProcessPlatform;
           yield* Console.log(
             platform === "darwin"
-              ? "\n✓ Background service ready\n\nT3 Code will stay reachable while you are logged in to this Mac."
-              : "\n✓ Background service ready\n\nT3 Code will stay reachable after you log out.",
+              ? "\n✓ Background service ready\n\nT3 Code is set to run while you are logged in to this Mac. The server establishes the T3 Connect link on startup."
+              : "\n✓ Background service ready\n\nT3 Code is set to keep running after you log out. The server establishes the T3 Connect link on startup.",
           );
           return;
         }

apps/server/src/cloud/relayResponse.test.ts
@@ -1,0 +1,145 @@
+import { expect, it } from "@effect/vitest";
+import * as Effect from "effect/Effect";
+import { HttpClientRequest, HttpClientResponse } from "effect/unstable/http";
+import * as HttpClientError from "effect/unstable/http/HttpClientError";
+
+import { filterRelayResponse, relayRequestError, shouldRetryCloudLink } from "./relayResponse.ts";
+
+const response = (
+  status: number,
+  body: string | Record<string, unknown>,
+  headers?: Record<string, string>,
+) =>
+  HttpClientResponse.fromWeb(
+    HttpClientRequest.post("https://relay.example.test/v1/client/environment-links"),
+    typeof body === "string"
+      ? new Response(body, { status, ...(headers ? { headers } : {}) })
+      : Response.json(body, { status, ...(headers ? { headers } : {}) }),
+  );
+
+it("reports a transport failure category without exposing request or cause details", () => {
+  const error = relayRequestError(
+    new HttpClientError.HttpClientError({
+      reason: new HttpClientError.TransportError({
+        request: HttpClientRequest.post("https://relay.example.test/link?token=private-token"),
+        description: "private transport details",
+        cause: new Error("private cause details"),
+      }),
+    }),
+  );
+
+  expect(error._tag).toBe("EnvironmentHttpInternalServerError");
+  expect(error.message).toContain("TransportError");
+  expect(error.message).toContain("network connection");
+  expect(error.message).not.toContain("relay.example.test");
+  expect(error.message).not.toContain("private");
+  expect(shouldRetryCloudLink(error)).toBe(true);
+});
+
+it.effect("reports the tunnel limit and relay trace instead of a generic 403", () =>
+  Effect.gen(function* () {
+    const error = yield* filterRelayResponse(
+      response(403, {
+        _tag: "RelayEnvironmentLinkLimitExceededError",
+        code: "environment_link_limit_exceeded",
+        maxTunnels: 3,
+        traceId: "trace-limit",
+      }),
+    ).pipe(Effect.mapError(relayRequestError), Effect.flip);
+
+    expect(error._tag).toBe("EnvironmentHttpForbiddenError");
+    expect(error.message).toContain("at most 3 tunnels");
+    expect(error.message).toContain("Unlink an unused environment");
+    expect(error.message).toContain("Trace ID: trace-limit");
+  }),
+);
+
+it.effect("makes revoked authorization actionable and non-retryable", () =>
+  Effect.gen(function* () {
+    const error = yield* filterRelayResponse(
+      response(401, {
+        _tag: "RelayAuthInvalidError",
+        code: "auth_invalid",
+        reason: "invalid_bearer",
+        traceId: "trace-auth",
+      }),
+    ).pipe(Effect.mapError(relayRequestError), Effect.flip);
+
+    expect(error._tag).toBe("EnvironmentHttpUnauthorizedError");
+    expect(error.message).toContain("invalid_bearer");
+    expect(error.message).toContain("t3 connect login");
+    expect(error.message).toContain("Trace ID: trace-auth");
+  }),
+);
+
+it.effect("reports an unrecognized access denial without printing its response body", () =>
+  Effect.gen(function* () {
+    const error = yield* filterRelayResponse(
+      response(403, "<html>private upstream details</html>", {
+        "content-type": "text/html",
+        "cf-ray": "abcdef1234-IAD",
+      }),
+    ).pipe(Effect.flip);
+
+    expect(error._tag).toBe("EnvironmentHttpForbiddenError");
+    expect(error.message).toContain("HTTP 403");
+    expect(error.message).toContain("proxy or firewall");
+    expect(error.message).toContain("Cloudflare Ray ID: abcdef1234-IAD");
+    expect(error.message).not.toContain("private upstream details");
+  }),
+);
+
+it.effect.each([408, 429, 500, 502, 503, 504])(
+  "keeps transient HTTP %s failures retryable",
+  (status) =>
+    Effect.gen(function* () {
+      const error = yield* filterRelayResponse(response(status, "unavailable")).pipe(Effect.flip);
+      expect(error._tag).toBe("EnvironmentHttpInternalServerError");
+      expect(error.message).toContain(`HTTP ${status}`);
+    }),
+);
+
+it.effect.each([
+  { status: 401, attempts: 1 },
+  { status: 403, attempts: 1 },
+  { status: 429, attempts: 2 },
+  { status: 503, attempts: 2 },
+])("stops rejected startup links but retries temporary failures: $status", ({ status, attempts }) =>
+  Effect.gen(function* () {
+    let requests = 0;
+    const result = yield* Effect.suspend(() => {
+      requests++;
+      return filterRelayResponse(response(requests === 1 ? status : 200, "{}"));
+    }).pipe(
+      Effect.mapError(relayRequestError),
+      Effect.retry({ while: shouldRetryCloudLink, times: 1 }),
+      Effect.result,
+    );
+    expect(requests).toBe(attempts);
+    expect(result._tag).toBe(attempts === 1 ? "Failure" : "Success");
+  }),
+);
+
+it.effect("keeps the relay failure reason and trace when tunnel cleanup fails", () =>
+  Effect.gen(function* () {
+    const error = yield* filterRelayResponse(
+      response(500, {
+        _tag: "RelayInternalError",
+        code: "internal_error",
+        reason: "upstream_unavailable",
+        traceId: "trace-cleanup",
+      }),
+    ).pipe(Effect.flip);
+
+    expect(error._tag).toBe("EnvironmentHttpInternalServerError");
+    expect(error.message).toContain("upstream_unavailable");
+    expect(error.message).toContain("Trace ID: trace-cleanup");
+  }),
+);
+
+it.effect("leaves successful response bodies available to their decoder", () =>
+  Effect.gen(function* () {
+    const result = yield* filterRelayResponse(response(200, '{"ok":true}'));
+    expect(yield* result.json).toEqual({ ok: true });
+  }),
+);

apps/server/src/cloud/relayResponse.ts
@@ -1,0 +1,82 @@
+import {
+  EnvironmentHttpBadRequestError,
+  EnvironmentHttpConflictError,
+  EnvironmentHttpForbiddenError,
+  EnvironmentHttpInternalServerError,
+  EnvironmentHttpUnauthorizedError,
+} from "@t3tools/contracts";
+import { RelayProtectedError } from "@t3tools/contracts/relay";
+import * as Effect from "effect/Effect";
+import * as Option from "effect/Option";
+import * as Schema from "effect/Schema";
+import * as HttpClientResponse from "effect/unstable/http/HttpClientResponse";
+import { isHttpClientError } from "effect/unstable/http/HttpClientError";
+
+const isRelayResponseError = Schema.is(
+  Schema.Union([
+    EnvironmentHttpBadRequestError,
+    EnvironmentHttpForbiddenError,
+    EnvironmentHttpInternalServerError,
+    EnvironmentHttpUnauthorizedError,
+  ]),
+);
+
+export function relayRequestError(cause: unknown) {
+  return isRelayResponseError(cause)
+    ? cause
+    : new EnvironmentHttpInternalServerError({
+        message: `Could not complete the T3 Connect relay request. ${isHttpClientError(cause) ? `The relay request failed (${cause.reason._tag}).` : "The relay returned an unexpected response."} Check this machine's network connection and relay availability, then retry.`,
+      });
+}
+
+const isPermanentCloudLinkError = Schema.is(
+  Schema.Union([
+    EnvironmentHttpBadRequestError,
+    EnvironmentHttpForbiddenError,
+    EnvironmentHttpUnauthorizedError,
+    EnvironmentHttpConflictError,
+  ]),
+);
+
+export const shouldRetryCloudLink = (error: unknown): boolean => !isPermanentCloudLinkError(error);
+
+function recoveryHint(error: RelayProtectedError): string {
+  switch (error._tag) {
+    case "RelayEnvironmentLinkLimitExceededError":
+      return "Unlink an unused environment in T3 Connect, then restart T3 Code on this machine.";
+    case "RelayAuthInvalidError":
+      return "Run `t3 connect login` to check this machine's authorization. If the stored credential was revoked, sign out with `t3 connect logout`, then run `t3 connect` again. Restart T3 Code after signing in.";
+    case "RelayEnvironmentLinkProofExpiredError":
+    case "RelayEnvironmentLinkProofInvalidError":
+      return "Check this machine's date and time, update T3 Code, then restart it.";
+    default:
+      return "Retry when the relay is available. If this continues, include the trace ID when reporting it.";
+  }
+}
+
+/** Preserve relay diagnostics before converting permanent rejections into non-retryable errors. */
+export const filterRelayResponse = Effect.fn("cloud.filter_relay_response")(function* (
+  response: HttpClientResponse.HttpClientResponse,
+) {
+  if (response.status >= 200 && response.status < 300) return response;
+  const decoded = yield* HttpClientResponse.schemaBodyJson(RelayProtectedError)(response).pipe(
+    Effect.option,
+  );
+  const ray = response.headers["cf-ray"];
+  const requestId = ray && /^[a-zA-Z0-9-]{1,128}$/.test(ray) ? ` Cloudflare Ray ID: ${ray}.` : "";
+  const message = Option.isSome(decoded)
+    ? `T3 Connect: ${decoded.value.message}. ${recoveryHint(decoded.value)} Trace ID: ${decoded.value.traceId}.`
+    : `T3 Connect relay returned HTTP ${response.status} without a recognized error response. Check relay access and any proxy or firewall restrictions, then restart T3 Code.${requestId}`;
+
+  if (response.status === 401) return yield* new EnvironmentHttpUnauthorizedError({ message });
+  if (response.status === 403) return yield* new EnvironmentHttpForbiddenError({ message });
+  if (
+    response.status >= 400 &&
+    response.status < 500 &&
+    response.status !== 408 &&
+    response.status !== 429
+  ) {
+    return yield* new EnvironmentHttpBadRequestError({ message });
+  }
+  return yield* new EnvironmentHttpInternalServerError({ message });
+});

richardsolomou/ras-code

(cherry picked from commit 99e3b721c5255ded20b00ba1798f848bfc0f1f65)

apps/server/src/cli/connect.ts
@@ -29,6 +29,7 @@ import * as ServerSecretStore from "../auth/ServerSecretStore.ts";
 import * as BootService from "../cloud/bootService.ts";
 import * as CliState from "../cloud/CliState.ts";
 import * as CliTokenManager from "../cloud/CliTokenManager.ts";
+import { filterRelayResponse } from "../cloud/relayResponse.ts";
 import {
   CLOUD_LINKED_USER_ID,
   isAgentActivityPublishingEnabledValue,
@@ -175,6 +176,8 @@ function formatCloudStatus(status: CloudCliStatus, options?: { readonly json?: b
     `  Environment link: ${provisioned}`,
     `  Relay: ${status.relayUrl ?? "not provisioned"}`,
     `  Publish agent activity: ${status.publishAgentActivity ? "enabled" : "disabled"}`,
+    "",
+    "This is saved setup, not a live connection check. Check the background service with `ras service status`.",
     ...(nextStep ? ["", `Next: ${nextStep}`] : []),
   ].join("\n");
 }
@@ -261,7 +264,7 @@ const unlinkRelayEnvironment = Effect.fn("cloud.cli.unlink_relay_environment")(f
   ).pipe(
     HttpClientRequest.bearerToken(token.value.accessToken),
     httpClient.execute,
-    Effect.flatMap(HttpClientResponse.filterStatusOk),
+    Effect.flatMap(filterRelayResponse),
     Effect.flatMap(HttpClientResponse.schemaBodyJson(RelayOkResponse)),
     withRelayClientTracing,
   );
@@ -578,17 +581,17 @@ export const connectCommand = Command.make("connect", {
         // Show which account was linked so an unexpected identity (an
         // authorization code for a different account) is visible before the
         // machine is brought online.
-        yield* Console.log(`✓ Connected${connectedAs(linked.identity)}`);
+        yield* Console.log(`✓ Authorized${connectedAs(linked.identity)}`);
 
-        // Connect itself already succeeded; a boot-service failure must not
-        // fail the command, just tell the user what happened and move on.
+        // Authorization is stored. If service setup fails, preserve it and
+        // show how to run the server manually.
         const background = yield* recoverServiceOnboardingOffer(offerServiceDuringOnboarding);
         if (background) {
           const platform = yield* HostProcessPlatform;
           yield* Console.log(
             platform === "darwin"
-              ? "\n✓ Background service ready\n\nRAS Code will stay reachable while you are logged in to this Mac."
-              : "\n✓ Background service ready\n\nRAS Code will stay reachable after you log out.",
+              ? "\n✓ Background service ready\n\nRAS Code is set to run while you are logged in to this Mac. The server establishes the RAS Connect link on startup."
+              : "\n✓ Background service ready\n\nRAS Code is set to keep running after you log out. The server establishes the RAS Connect link on startup.",
           );
           return;
         }

apps/server/src/cloud/relayResponse.test.ts
@@ -1,0 +1,145 @@
+import { expect, it } from "@effect/vitest";
+import * as Effect from "effect/Effect";
+import { HttpClientRequest, HttpClientResponse } from "effect/unstable/http";
+import * as HttpClientError from "effect/unstable/http/HttpClientError";
+
+import { filterRelayResponse, relayRequestError, shouldRetryCloudLink } from "./relayResponse.ts";
+
+const response = (
+  status: number,
+  body: string | Record<string, unknown>,
+  headers?: Record<string, string>,
+) =>
+  HttpClientResponse.fromWeb(
+    HttpClientRequest.post("https://relay.example.test/v1/client/environment-links"),
+    typeof body === "string"
+      ? new Response(body, { status, ...(headers ? { headers } : {}) })
+      : Response.json(body, { status, ...(headers ? { headers } : {}) }),
+  );
+
+it("reports a transport failure category without exposing request or cause details", () => {
+  const error = relayRequestError(
+    new HttpClientError.HttpClientError({
+      reason: new HttpClientError.TransportError({
+        request: HttpClientRequest.post("https://relay.example.test/link?token=private-token"),
+        description: "private transport details",
+        cause: new Error("private cause details"),
+      }),
+    }),
+  );
+
+  expect(error._tag).toBe("EnvironmentHttpInternalServerError");
+  expect(error.message).toContain("TransportError");
+  expect(error.message).toContain("network connection");
+  expect(error.message).not.toContain("relay.example.test");
+  expect(error.message).not.toContain("private");
+  expect(shouldRetryCloudLink(error)).toBe(true);
+});
+
+it.effect("reports the tunnel limit and relay trace instead of a generic 403", () =>
+  Effect.gen(function* () {
+    const error = yield* filterRelayResponse(
+      response(403, {
+        _tag: "RelayEnvironmentLinkLimitExceededError",
+        code: "environment_link_limit_exceeded",
+        maxTunnels: 3,
+        traceId: "trace-limit",
+      }),
+    ).pipe(Effect.mapError(relayRequestError), Effect.flip);
+
+    expect(error._tag).toBe("EnvironmentHttpForbiddenError");
+    expect(error.message).toContain("at most 3 tunnels");
+    expect(error.message).toContain("Unlink an unused environment");
+    expect(error.message).toContain("Trace ID: trace-limit");
+  }),
+);
+
+it.effect("makes revoked authorization actionable and non-retryable", () =>
+  Effect.gen(function* () {
+    const error = yield* filterRelayResponse(
+      response(401, {
+        _tag: "RelayAuthInvalidError",
+        code: "auth_invalid",
+        reason: "invalid_bearer",
+        traceId: "trace-auth",
+      }),
+    ).pipe(Effect.mapError(relayRequestError), Effect.flip);
+
+    expect(error._tag).toBe("EnvironmentHttpUnauthorizedError");
+    expect(error.message).toContain("invalid_bearer");
+    expect(error.message).toContain("ras connect login");
+    expect(error.message).toContain("Trace ID: trace-auth");
+  }),
+);
+
+it.effect("reports an unrecognized access denial without printing its response body", () =>
+  Effect.gen(function* () {
+    const error = yield* filterRelayResponse(
+      response(403, "<html>private upstream details</html>", {
+        "content-type": "text/html",
+        "cf-ray": "abcdef1234-IAD",
+      }),
+    ).pipe(Effect.flip);
+
+    expect(error._tag).toBe("EnvironmentHttpForbiddenError");
+    expect(error.message).toContain("HTTP 403");
+    expect(error.message).toContain("proxy or firewall");
+    expect(error.message).toContain("Cloudflare Ray ID: abcdef1234-IAD");
+    expect(error.message).not.toContain("private upstream details");
+  }),
+);
+
+it.effect.each([408, 429, 500, 502, 503, 504])(
+  "keeps transient HTTP %s failures retryable",
+  (status) =>
+    Effect.gen(function* () {
+      const error = yield* filterRelayResponse(response(status, "unavailable")).pipe(Effect.flip);
+      expect(error._tag).toBe("EnvironmentHttpInternalServerError");
+      expect(error.message).toContain(`HTTP ${status}`);
+    }),
+);
+
+it.effect.each([
+  { status: 401, attempts: 1 },
+  { status: 403, attempts: 1 },
+  { status: 429, attempts: 2 },
+  { status: 503, attempts: 2 },
+])("stops rejected startup links but retries temporary failures: $status", ({ status, attempts }) =>
+  Effect.gen(function* () {
+    let requests = 0;
+    const result = yield* Effect.suspend(() => {
+      requests++;
+      return filterRelayResponse(response(requests === 1 ? status : 200, "{}"));
+    }).pipe(
+      Effect.mapError(relayRequestError),
+      Effect.retry({ while: shouldRetryCloudLink, times: 1 }),
+      Effect.result,
+    );
+    expect(requests).toBe(attempts);
+    expect(result._tag).toBe(attempts === 1 ? "Failure" : "Success");
+  }),
+);
+
+it.effect("keeps the relay failure reason and trace when tunnel cleanup fails", () =>
+  Effect.gen(function* () {
+    const error = yield* filterRelayResponse(
+      response(500, {
+        _tag: "RelayInternalError",
+        code: "internal_error",
+        reason: "upstream_unavailable",
+        traceId: "trace-cleanup",
+      }),
+    ).pipe(Effect.flip);
+
+    expect(error._tag).toBe("EnvironmentHttpInternalServerError");
+    expect(error.message).toContain("upstream_unavailable");
+    expect(error.message).toContain("Trace ID: trace-cleanup");
+  }),
+);
+
+it.effect("leaves successful response bodies available to their decoder", () =>
+  Effect.gen(function* () {
+    const result = yield* filterRelayResponse(response(200, '{"ok":true}'));
+    expect(yield* result.json).toEqual({ ok: true });
+  }),
+);

apps/server/src/cloud/relayResponse.ts
@@ -1,0 +1,82 @@
+import {
+  EnvironmentHttpBadRequestError,
+  EnvironmentHttpConflictError,
+  EnvironmentHttpForbiddenError,
+  EnvironmentHttpInternalServerError,
+  EnvironmentHttpUnauthorizedError,
+} from "@t3tools/contracts";
+import { RelayProtectedError } from "@t3tools/contracts/relay";
+import * as Effect from "effect/Effect";
+import * as Option from "effect/Option";
+import * as Schema from "effect/Schema";
+import * as HttpClientResponse from "effect/unstable/http/HttpClientResponse";
+import { isHttpClientError } from "effect/unstable/http/HttpClientError";
+
+const isRelayResponseError = Schema.is(
+  Schema.Union([
+    EnvironmentHttpBadRequestError,
+    EnvironmentHttpForbiddenError,
+    EnvironmentHttpInternalServerError,
+    EnvironmentHttpUnauthorizedError,
+  ]),
+);
+
+export function relayRequestError(cause: unknown) {
+  return isRelayResponseError(cause)
+    ? cause
+    : new EnvironmentHttpInternalServerError({
+        message: `Could not complete the RAS Connect relay request. ${isHttpClientError(cause) ? `The relay request failed (${cause.reason._tag}).` : "The relay returned an unexpected response."} Check this machine's network connection and relay availability, then retry.`,
+      });
+}
+
+const isPermanentCloudLinkError = Schema.is(
+  Schema.Union([
+    EnvironmentHttpBadRequestError,
+    EnvironmentHttpForbiddenError,
+    EnvironmentHttpUnauthorizedError,
+    EnvironmentHttpConflictError,
+  ]),
+);
+
+export const shouldRetryCloudLink = (error: unknown): boolean => !isPermanentCloudLinkError(error);
+
+function recoveryHint(error: RelayProtectedError): string {
+  switch (error._tag) {
+    case "RelayEnvironmentLinkLimitExceededError":
+      return "Unlink an unused environment in RAS Connect, then restart RAS Code on this machine.";
+    case "RelayAuthInvalidError":
+      return "Run `ras connect login` to check this machine's authorization. If the stored credential was revoked, sign out with `ras connect logout`, then run `ras connect` again. Restart RAS Code after signing in.";
+    case "RelayEnvironmentLinkProofExpiredError":
+    case "RelayEnvironmentLinkProofInvalidError":
+      return "Check this machine's date and time, update RAS Code, then restart it.";
+    default:
+      return "Retry when the relay is available. If this continues, include the trace ID when reporting it.";
+  }
+}
+
+/** Preserve relay diagnostics before converting permanent rejections into non-retryable errors. */
+export const filterRelayResponse = Effect.fn("cloud.filter_relay_response")(function* (
+  response: HttpClientResponse.HttpClientResponse,
+) {
+  if (response.status >= 200 && response.status < 300) return response;
+  const decoded = yield* HttpClientResponse.schemaBodyJson(RelayProtectedError)(response).pipe(
+    Effect.option,
+  );
+  const ray = response.headers["cf-ray"];
+  const requestId = ray && /^[a-zA-Z0-9-]{1,128}$/.test(ray) ? ` Cloudflare Ray ID: ${ray}.` : "";
+  const message = Option.isSome(decoded)
+    ? `RAS Connect: ${decoded.value.message}. ${recoveryHint(decoded.value)} Trace ID: ${decoded.value.traceId}.`
+    : `RAS Connect relay returned HTTP ${response.status} without a recognized error response. Check relay access and any proxy or firewall restrictions, then restart RAS Code.${requestId}`;
+
+  if (response.status === 401) return yield* new EnvironmentHttpUnauthorizedError({ message });
+  if (response.status === 403) return yield* new EnvironmentHttpForbiddenError({ message });
+  if (
+    response.status >= 400 &&
+    response.status < 500 &&
+    response.status !== 408 &&
+    response.status !== 429
+  ) {
+    return yield* new EnvironmentHttpBadRequestError({ message });
+  }
+  return yield* new EnvironmentHttpInternalServerError({ message });
+});